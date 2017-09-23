1e29cca5711a9c7d98f41a134bcb1342a04615d7
NuclearFukuRo <nuclear.fukuro@gmail.com>
2017-09-23 09:28:17 +0300

    change .gitignore

---
+++ src/ru/fukuro/cells/main/Main.java
@@ -0,0 +1,10 @@
+package ru.fukuro.cells.main;
+
+/**
+ * Created by FukuRo on 23.09.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ZIEG HAIL");
+    }
+}
